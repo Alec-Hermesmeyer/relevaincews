d8688a37068dd43e736bf6a824990a0d247df261
Alec Hermesmeyer <ahrmsmyr@gmail.com>
2024-11-01 14:07:15 -0500

    footer change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -402,8 +402,8 @@ export default function ModernLandingPage() {
         </main>
         <footer className="bg-gray-800  py-8">
           <div className="container mx-auto px-2">
-            <div className="flex flex-col md:flex-row justify-end items-center">
-              <p className="text-sm text-white mb-4 md:mb-0 text-right">
+            <div className="flex flex-col md:flex-row justify-center items-center">
+              <p className="text-sm text-white mb-4 md:mb-0 text-center">
                 RelevAInce is a registered trademark of RelevAInce LLC (US Serial No. 98/165,416) © 2024 relevAInce All rights reserved.
               </p>
 
