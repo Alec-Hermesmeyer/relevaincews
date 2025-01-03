2c52074ecd605ce02cf0a577ae4be3d782316e0d
Alec Hermesmeyer <ahrmsmyr@gmail.com>
2025-01-03 15:00:55 -0600

    commit

--- src/components/BusinessCard.tsx
+++ src/components/BusinessCard.tsx
@@ -43,37 +43,37 @@ const BusinessCard: React.FC<BusinessCardProps> = ({
 //     URL.revokeObjectURL(url);
 //   };
 
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-black">
-               
-
-      <div
-        className="p-8 ml-2 mr-2  border rounded-lg shadow-lg max-w-md w-80 h-[65dvh] text-center text-white relative bg-[#10275E] overflow-hidden "
-        style={{
-           backgroundImage: `url('/LogoPurple.png')`,
-          backgroundSize: "cover",
-          backgroundPosition: "2% center", // Off-center the background image
-          backgroundColor: "#10275E", // Navy-600 Hex Color as fallback
-           backdropFilter: "blur(5px)",
-           backgroundRepeat: "no-repeat",
-         }}
-      >
-        <Image src='/Wordmark_wo_TM - White.ai (1).png' alt={name} height={120} width={200} className="" />
-         <div className=" p-6 rounded-md z-auto absolute bottom-20 -right-1 left-1">
-          
-          <h2 className="mt-6 text-5xl font-extrabold text-right mb-2 font-roboto ">{name}</h2>
-          <p className="text-xl font-semibold text-right mb-2">{title}</p>
-          <p className="text-xl font-semibold text-right">{email}</p>
-         
-          
-          
-        </div>
-       
-       
+return (
+  <div className="min-h-screen flex items-center justify-center bg-black">
+    <div
+      className="p-8 mx-2 border rounded-lg shadow-lg max-w-sm w-80 h-[65vh] text-center text-white relative bg-[#10275E] overflow-hidden"
+      style={{
+        backgroundImage: `url('/LogoPurple.png')`,
+        backgroundSize: 'cover',
+        backgroundPosition: '2% center', // Off-center the background image
+        backgroundColor: '#10275E', // Navy-600 Hex Color as fallback
+        backdropFilter: 'blur(5px)',
+        backgroundRepeat: 'no-repeat',
+        transition: 'none', // Disable any transitions
+      }}
+    >
+      <Image
+        src="/Wordmark_wo_TM - White.ai (1).png"
+        alt={name}
+        height={120}
+        width={200}
+        className=""
+      />
+      <div className="p-6 rounded-md z-auto absolute bottom-20 left-1 right-1">
+        <h2 className="mt-6 text-5xl font-extrabold text-right mb-2 font-roboto">
+          {name}
+        </h2>
+        <p className="text-xl font-semibold text-right mb-2">{title}</p>
+        <p className="text-xl font-semibold text-right">{email}</p>
       </div>
-     
     </div>
-  );
+  </div>
+);
 };
 
 export default BusinessCard;
